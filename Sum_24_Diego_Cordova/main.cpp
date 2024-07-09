4bcdba47c9e28b7264c73fa11434e1ecae4cfb10
dcordova7146 <diego.cordova79@myhunter.cuny.edu>
2024-07-08 21:15:14 -0400

    Input implemented, cmake updated

--- Sum_24_Diego_Cordova/main.cpp
+++ Sum_24_Diego_Cordova/main.cpp
@@ -14,7 +14,6 @@ public:
 	virtual void OnUpdate() override
 	{
 		Dino::Renderer::Get()->Draw(pic, x, 200);
-		x += 1;
 	}
 
 private:
@@ -23,7 +22,7 @@ private:
 	int x{ 100 };
 
 	void MyKeyPressedCallback(const Dino::KeyPressedEvent& key) {
-		if (key.GetKey() == DINO_KEY_UP)
+		if (key.GetKey() == DINO_KEY_RIGHT)
 			x += 40;
 	}
 };
